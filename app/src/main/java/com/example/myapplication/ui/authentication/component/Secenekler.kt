6939e20d2e6c65fd6c3def9d3015db06c1978c27
eminegunes <emine.lalegns@gmail.com>
2024-04-22 16:06:38 +0300

    refactor code

--- app/src/main/java/com/example/myapplication/ui/authentication/component/Secenekler.kt
+++ app/src/main/java/com/example/myapplication/ui/authentication/component/Secenekler.kt
@@ -1,12 +1,13 @@
-package com.example.myapplication
+package com.example.myapplication.ui.authentication.component
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
 
-class secenekler : AppCompatActivity() {
+class Secenekler : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
