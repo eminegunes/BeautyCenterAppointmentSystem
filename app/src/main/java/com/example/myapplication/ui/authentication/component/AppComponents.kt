2f817946a4821dffe84c7e94509cb7bfa42408c3
eminegunes <emine.lalegns@gmail.com>
2024-05-03 12:28:51 +0300

    added custom compose components

---
+++ app/src/main/java/com/example/myapplication/ui/authentication/component/AppComponents.kt
@@ -0,0 +1,318 @@
+package com.example.myapplication.ui.authentication.component
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.myapplication.R
+import com.example.myapplication.ui.theme.AccentColor
+import com.example.myapplication.ui.theme.BgColor
+import com.example.myapplication.ui.theme.GrayColor
+import com.example.myapplication.ui.theme.Primary
+import com.example.myapplication.ui.theme.Secondary
+import com.example.myapplication.ui.theme.TextColor
+
+
+@Composable
+fun NormalTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp),
+        style = TextStyle(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Normal,
+            fontStyle = FontStyle.Normal
+        ),
+        color = TextColor,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun HeadingTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal
+        ),
+        color = TextColor,
+        textAlign = TextAlign.Center
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextFieldComponent(labelValue: String, icon: ImageVector) {
+    var textValue by remember {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        label = {
+            Text(text = labelValue)
+        },
+        value = textValue,
+        onValueChange = {
+            textValue = it
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = AccentColor,
+            focusedLabelColor = AccentColor,
+            cursorColor = Primary,
+            containerColor = BgColor,
+            focusedLeadingIconColor = AccentColor,
+        ),
+        modifier = Modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        leadingIcon = {
+            Icon(
+                imageVector = icon,
+                contentDescription = "profile"
+            )
+        },
+        keyboardOptions = KeyboardOptions.Default
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordTextFieldComponent(labelValue: String, icon: ImageVector) {
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+    OutlinedTextField(
+        label = {
+            Text(text = labelValue)
+        },
+        value = password,
+        onValueChange = {
+            password = it
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = AccentColor,
+            focusedLabelColor = AccentColor,
+            cursorColor = Primary,
+            containerColor = BgColor,
+            focusedLeadingIconColor = AccentColor,
+        ),
+        modifier = Modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        leadingIcon = {
+            Icon(
+                imageVector = icon,
+                contentDescription = "profile"
+            )
+        },
+        trailingIcon = {
+            val iconImage =
+                if (isPasswordVisible) Icons.Outlined.Warning else Icons.Outlined.AccountCircle
+            val description = if (isPasswordVisible) "Show Password" else "Hide Password"
+            IconButton(onClick = {
+                isPasswordVisible = !isPasswordVisible
+            }) {
+                Icon(imageVector = iconImage, contentDescription = description)
+            }
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
+    )
+}
+
+@Composable
+fun CheckboxComponent() {
+    var isChecked by remember {
+        mutableStateOf(false)
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(56.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = {
+                isChecked = it
+            }
+        )
+        ClickableTextComponent()
+    }
+}
+
+@Composable
+fun ClickableTextComponent() {
+    val initialText = "Devam ederek şunları kabul edersiniz: "
+    val privacyPolicyText = "Gizlilik Politikası"
+    val andText = " ve "
+    val termOfUseText = "Kullanım Koşulları"
+
+
+    val annotatedString = buildAnnotatedString {
+        withStyle(style = SpanStyle(color = TextColor)) {
+            append(initialText)
+        }
+        withStyle(style = SpanStyle(color = Secondary)) {
+            pushStringAnnotation(tag = privacyPolicyText, annotation = privacyPolicyText)
+            append(privacyPolicyText)
+        }
+        withStyle(style = SpanStyle(color = TextColor)) {
+            append(andText)
+        }
+        withStyle(style = SpanStyle(color = Secondary)) {
+            pushStringAnnotation(tag = termOfUseText, annotation = termOfUseText)
+            append(termOfUseText)
+        }
+        append(".")
+    }
+
+    ClickableText(text = annotatedString, onClick = {
+        annotatedString.getStringAnnotations(it, it)
+            .firstOrNull()?.also { annotation ->
+                Log.d("ClickableTextComponent", "You have Clicked ${annotation.tag}")
+            }
+    })
+}
+
+@Composable
+fun BottomComponent(
+    textQuery: String,
+    textClickable: String,
+    action: String,
+    navController: NavHostController
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(Color.Transparent)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .background(
+                            brush = Brush.horizontalGradient(listOf(Secondary, AccentColor)),
+                            shape = RoundedCornerShape(50.dp)
+                        )
+                        .fillMaxWidth()
+                        .heightIn(48.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = action, color = Color.White, fontSize = 20.sp)
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                HorizontalDivider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    thickness = 1.dp,
+                    color = GrayColor
+                )
+
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+            AccountQueryComponent(textQuery, textClickable, navController)
+        }
+    }
+}
+
+@Composable
+fun AccountQueryComponent(
+    textQuery: String,
+    textClickable: String,
+    navController: NavHostController
+) {
+    val annonatedString = buildAnnotatedString {
+        withStyle(style = SpanStyle(color = TextColor, fontSize = 15.sp)) {
+            append(textQuery)
+        }
+        withStyle(style = SpanStyle(color = Secondary, fontSize = 15.sp)) {
+            pushStringAnnotation(tag = textClickable, annotation = textClickable)
+            append(textClickable)
+        }
+    }
+
+    ClickableText(text = annonatedString, onClick = {
+        annonatedString.getStringAnnotations(it, it)
+            .firstOrNull()?.also { annonation ->
+                if (annonation.item == "Giriş yap") {
+                    navController.navigate("Login")
+                } else if (annonation.item == "Kayıt ol") {
+                    navController.navigate("Signup")
+                }
+            }
+    })
+}
