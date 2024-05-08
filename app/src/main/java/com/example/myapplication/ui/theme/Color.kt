bed67c6e1b2ce4636e4736df17101a67cc97d44d
eminegunes <emine.lalegns@gmail.com>
2024-05-08 18:22:46 +0300

    added app color

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Primary = Color(0xFF2A3FD)
+val RedApp = Color(0xFFE60A55)
 val Secondary = Color(0xFF9DCEFF)
 val TextColor = Color(0xFF1D1617)
 val AccentColor = Color(0xFFC58BF2)
