f8ebe6d46f9f71b9b1e6ee78f1ffc3d13c0bc1dd
eminegunes <emine.lalegns@gmail.com>
2024-05-08 18:25:32 +0300

    created mock home page

---
+++ app/src/main/java/com/example/myapplication/ui/main_screens/Home.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.ui.main_screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+
+fun Home() {
+    Column(
+
+    ) {
+        Text(text = "Home")
+    }
+}
