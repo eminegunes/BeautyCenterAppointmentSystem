c8af27b79adb768caa9a9a4b814387bcb8562c62
eminegunes <emine.lalegns@gmail.com>
2024-05-08 18:27:55 +0300

    cratering Network Repository

---
+++ app/src/main/java/com/example/myapplication/di/NetworkModule.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.di
+
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return Gson()
+    }
+}
