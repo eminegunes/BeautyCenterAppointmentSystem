ddf68863d0d6fc11ccc86a585c0e37b2ff1b357b
eminegunes <emine.lalegns@gmail.com>
2024-05-08 18:24:17 +0300

    refactoring AuthMainScreen

--- app/src/main/java/com/example/myapplication/ui/authentication/main_screen/AuthMainScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/authentication/main_screen/AuthMainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.navigation.Navigation
 
 @Composable
@@ -13,6 +14,8 @@ fun AuthMainScreen() {
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
-        Navigation()
+
+        val navController = rememberNavController()
+        Navigation(navController = navController)
     }
 }
