d4c555bee1e46bdfa8a381c51c10fe2c82964502
eminegunes <emine.lalegns@gmail.com>
2024-05-23 15:58:55 +0300

    added bottombar to home screen

--- app/src/main/java/com/example/myapplication/ui/main_screens/Home.kt
+++ app/src/main/java/com/example/myapplication/ui/main_screens/Home.kt
@@ -1,15 +1,48 @@
 package com.example.myapplication.ui.main_screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.ui.main_screens.home.componet.BottomBar
+import com.example.myapplication.ui.navigation.BottomBarScreen
+import com.example.myapplication.ui.navigation.NavGraph
 
 @Composable
+fun HomeScreen(
+) {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
 
-fun Home() {
-    Column(
+    Scaffold(
+        bottomBar = {
+            if (currentRoute == BottomBarScreen.Home.route || currentRoute == BottomBarScreen.Notifications.route || currentRoute == BottomBarScreen.DateRange.route) {
+                BottomAppBar(
+                    modifier = Modifier
+                        .height(50.dp),
+                ) {
+                    BottomBar(navController = navController)
+                }
+            }
+        }
+    ) { padding ->
+        Box(modifier = Modifier.padding(padding)) {
+            NavGraph(
+                navController = navController,
+                startDestination = BottomBarScreen.Home.route,
 
-    ) {
-        Text(text = "Home")
+            )
+        }
+
     }
 }
